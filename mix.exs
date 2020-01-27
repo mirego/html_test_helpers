8395ba346a5bbd04743da1e6f785e29c08823513
romarickb <rboley@mirego.com>
2020-01-27 09:49:08 -0500

    Add project medatada (#4)
    * Add project medatada
    
    * Add ex_doc dependency
    
    * Remove organization from package fields and fix typo

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule HTMLTestHelpers.MixProject do
       version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/mirego/html_test_helpers"
     ]
   end
 
@@ -24,7 +27,19 @@ defmodule HTMLTestHelpers.MixProject do
 
       # Linting
       {:credo, "~> 1.1", only: [:dev, :test], override: true},
-      {:credo_naming, "~> 0.4", only: [:dev, :test], runtime: false}
+      {:credo_naming, "~> 0.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "HTMLTestHelpers provides function helpers for unit testing allowing easy assertions for HTML elements data queried by data-testid attribute."
+  end
+
+  defp package do
+    [
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => "https://github.com/mirego/html_test_helpers"}
     ]
   end
 end
